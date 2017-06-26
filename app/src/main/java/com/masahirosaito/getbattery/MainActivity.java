2a16e86931872a8a1fa3b7b5497e1de7b069926f
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-27 01:07:09 +0900

    とりあえず適当に実装

--- app/src/main/java/com/masahirosaito/getbattery/MainActivity.java
+++ app/src/main/java/com/masahirosaito/getbattery/MainActivity.java
@@ -1,13 +1,65 @@
 package com.masahirosaito.getbattery;
 
-import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int batteryLevel;
+    float batteryTemperature;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int rawLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            if (rawLevel != -1 && scale != -1) {
+                float batteryLevelFloat = rawLevel / (float) scale;
+                batteryLevel = (int) (batteryLevelFloat * 100);
+            }
+
+            batteryTemperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10.0f;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.update_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        getApplicationContext().registerReceiver(broadcastReceiver, intentFilter);
+        update();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getApplicationContext().unregisterReceiver(broadcastReceiver);
+    }
+
+    private void update() {
+        ((TextView) findViewById(R.id.battery_level)).setText(String.valueOf(batteryLevel));
+        ((TextView) findViewById(R.id.battery_temperature)).setText(String.valueOf(batteryTemperature));
     }
 }
